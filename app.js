c25ca0ca41535912ad63cad9e35981ed569f7fcc
FloreaClaudiu <30389908+FloreaClaudiu21@users.noreply.github.com>
2023-01-01 10:22:35 +0200

    new backend code

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-require("dotenv").config();
-const path = require("path");
-const cors = require("cors");
-const express = require("express");
-const bodyParser = require("body-parser");
-//////////////////////////////////////////
-const app = express();
-const PORT = process.env.PORT || 8000;
-const Database = require("./public/js/sql");
-const publicDir = path.join(__dirname, "public");
-/////////////////////////////////////////////////
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// STATIC LINKS
-app.use(express.static(publicDir));
-///////////////////////////////////
-// CONNECT TO THE DATABASE
-const DATABASE = new Database();
-// APP ROUTES
-const { main_route: BandsController } = require("./routes/BandsController");
-const {
-	main_route: FeedbacksController,
-} = require("./routes/FeedbackController");
-const { main_route: AdminController } = require("./routes/AdminsController");
-app.use(BandsController);
-app.use(AdminController);
-app.use(FeedbacksController);
 // LISTENING TO THE PORT
+const {app, PORT} = require('./public/index');
 app.listen(PORT, () => console.log(`Listening on the port: ${PORT}`));
-module.exports = { DATABASE };
---
+++ public/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+const serverless = require('serverless-http');
+const bodyParser = require("body-parser");
+//////////////////////////////////////////
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 8000;
+const Database = require("./js/sql");
+const publicDir = path.join(__dirname, "public");
+/////////////////////////////////////////////////
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/.netlify/functions/server', router);
+// STATIC LINKS
+app.use(express.static(publicDir));
+///////////////////////////////////
+// CONNECT TO THE DATABASE
+const DATABASE = new Database();
+// APP ROUTES
+const { main_route: BandsController } = require("../routes/BandsController");
+const {
+	main_route: FeedbacksController,
+} = require("../routes/FeedbackController");
+const { main_route: AdminController } = require("../routes/AdminsController");
+app.use(BandsController);
+app.use(AdminController);
+app.use(FeedbacksController);
+module.exports = { DATABASE, app, PORT };
+module.exports.handler = serverless(app);
